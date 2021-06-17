438784e723446e6c8f8ffde97f2a4a67f7c597dd
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-17 12:40:49 +0200

    adds display of list of all courses

--- react-app/src/Courses.jsx
+++ react-app/src/Courses.jsx
@@ -4,7 +4,12 @@ import {courses} from './data/courses'
 const Courses = () => {
   const listOfCources = courses.map((course) => (
     <div data-cy='course-container'>
-      <h2>{course.title}</h2>
+      <h2 data-cy='title'>{course.title}</h2>
+      <p data-cy='description'>{course.description}</p>
+      <p data-cy='category' className='capitalized'><span className='bold'>Category: </span>{course.category}</p>
+      <p data-cy='instructors'><span className='bold'>Instructors: </span>{course.instructors}</p>
+      <p data-cy='info'>{course.info}</p>
+      <p data-cy='price'><span className='bold'>Price: </span>{course.price}</p>
     </div>
   ));
   return <>{listOfCources}</>;
