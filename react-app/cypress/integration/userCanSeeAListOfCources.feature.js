48d27ed4b15bfd8f947ec0675490f8dd5ded7e60
sevader14 <vsevolod.deriushkin@gmail.com>
2021-06-17 12:09:52 +0200

    adds tests for display of list of courses

--- react-app/cypress/integration/userCanSeeAListOfCources.feature.js
+++ react-app/cypress/integration/userCanSeeAListOfCources.feature.js
@@ -1,9 +1,28 @@
 describe('user can see app and a list of all cources', () => {
   beforeEach(() => {
-    cy.visit('/')
-  });  
-  
-  it('is expected to show page header', () => {
-    cy.get('[data-cy=page-header]').should('contain.text', 'Course list')
+    cy.visit('/');
   });
-})
+
+  it('is expected to show page header', () => {
+    cy.get('[data-cy=page-header]').should('contain.text', 'Course list');
+  });
+
+  it('is expected to show the list of categories', () => {
+    cy.get('[data-cy=category-menu]').within(() => {
+      cy.get('[data-cy=category]').should('have.length', 6);
+      cy.get('[data-cy=category]').first().shoud('contain.text', 'Workshops');
+    });
+  });
+
+  it('is expected to show the list of all courses', () => {
+    cy.get('[data-cy=course-container]').should('have.length', 7);
+    cy.get('[data-cy=course-container]').first().within(() => {
+      cy.get('[data-cy=title]').should('be.visible');
+      cy.get('[data-cy=description]').should('be.visible');
+      cy.get('[data-cy=category]').should('be.visible');
+      cy.get('[data-cy=instructors]').should('be.visible');
+      cy.get('[data-cy=info]').should('be.visible');
+      cy.get('[data-cy=price]').should('be.visible');
+    });
+  });
+});
